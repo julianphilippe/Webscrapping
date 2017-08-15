90fe8927c672b70af608f5f690d650a6ab5c9eec
julianphilippe <julian.philippe@outlook.com>
2017-08-14 23:09:53 -0300

    Update produtos.R

--- produtos.R
+++ produtos.R
@@ -41,17 +41,24 @@ buscarProdutosSubmarino <- function(produto){
   #localizar paradas do JSON
   parada_json <- as.data.frame(str_locate(data, '\\}\\}\\}')) 
   
+  index_na <- which(is.na(parada_json))
+  
   #excluir NAs
-  parada_json <- na.omit(parada_json)
+  parada_json <- parada_json %>%
+    filter(!is.na(start))
+  
+  data <- as.tibble(data) 
+  
+  data <- data[-index_na[1],]
+  
   
   #cria tibble de saída
   listaProdutos <- tibble()
   
-  
   for (i in 1:nrow(parada_json)){
     
     #carrega dados do produto em JSON
-    data_json <- fromJSON(str_sub(data[i], 1, parada_json$end[i]))
+    data_json <- fromJSON(str_sub(as.character(data[i,]), 1, parada_json$end[i]))
     
     #armazena valores em variável 
     x <- tibble(name = data_json$name,
@@ -61,8 +68,6 @@ buscarProdutosSubmarino <- function(produto){
     
     #carrega dados no tibble de saída
     listaProdutos <- rbind(listaProdutos,x)
-    
-    
   }
   
   return(listaProdutos)
@@ -70,7 +75,5 @@ buscarProdutosSubmarino <- function(produto){
 }
 
 
+teste <- buscarProdutosSubmarino('iphone 5')
 
-
-
-teste <- buscarProdutosSubmarino('the last of us')
